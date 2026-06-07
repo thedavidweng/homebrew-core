ccacf50468490dde91abf499934d2249a2607166
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-07 02:59:51 +0000

    ghalint 1.5.7-1

--- Formula/g/ghalint.rb
+++ Formula/g/ghalint.rb
@@ -1,8 +1,8 @@
 class Ghalint < Formula
   desc "GitHub Actions linter"
   homepage "https://github.com/suzuki-shunsuke/ghalint"
-  url "https://github.com/suzuki-shunsuke/ghalint/archive/refs/tags/v1.5.6.tar.gz"
-  sha256 "1188047b654a86390d49b776153c1a7b3eddde30ebcc0d024dfab9585785b02b"
+  url "https://github.com/suzuki-shunsuke/ghalint/archive/refs/tags/v1.5.7-1.tar.gz"
+  sha256 "b1748d0e601c24a3a6d9c8d6f6553a46dd178653027ea8060656b2bcd46d8327"
   license "MIT"
   head "https://github.com/suzuki-shunsuke/ghalint.git", branch: "main"
 
